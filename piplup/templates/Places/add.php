e78706aec98e8be166b344239b50c701dd217297
Nicolas Stevan Tan <nicolas562003@gmail.com>
2025-04-25 12:10:40 +1000

    Add new Place base UI

--- piplup/templates/Places/add.php
+++ piplup/templates/Places/add.php
@@ -5,16 +5,20 @@
  * @var \Cake\Collection\CollectionInterface|string[] $subcategories
  */
 ?>
+<?= $this->Html->link('Back', '/new-menu', ['class' => 'px-1 pt-1', 'escape' => false]) ?>
+<!-- Piplup Header -->
+<div class="d-flex flex-wrap justify-content-start align-items-center">
+    <?= $this->Html->image('piplup.png', [
+        'class' => 'mb-3',
+        'style' => 'width:113px; height:113px;',
+        'alt' => 'Piplup'
+    ]) ?>
+    <h1 class="h3 mb-3 heading-title">New Place?</h1>
+</div>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Places'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
     <div class="column column-80">
         <div class="places form content">
-            <?= $this->Form->create($place) ?>
+        <?= $this->Form->create($place) ?>
             <fieldset>
                 <legend><?= __('Add Place') ?></legend>
                 <?php
@@ -24,8 +28,8 @@
                     echo $this->Form->control('description');
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
+        <?= $this->Form->button(__('Submit')) ?>
+        <?= $this->Form->end() ?>
         </div>
     </div>
 </div>
