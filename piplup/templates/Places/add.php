989965875450885a971cf03224a3a17a4447cd34
Nicolas Stevan Tan <nicolas562003@gmail.com>
2025-04-25 21:49:49 +1000

    Places add UI

--- piplup/templates/Places/add.php
+++ piplup/templates/Places/add.php
@@ -6,6 +6,7 @@
  */
 ?>
 <?= $this->Html->link('Back', '/new-menu', ['class' => 'px-1 pt-1', 'escape' => false]) ?>
+
 <!-- Piplup Header -->
 <div class="d-flex flex-wrap justify-content-start align-items-center">
     <?= $this->Html->image('piplup.png', [
@@ -15,21 +16,54 @@
     ]) ?>
     <h1 class="h3 mb-3 heading-title">New Place?</h1>
 </div>
+
+<!-- Form Container -->
 <div class="row">
-    <div class="column column-80">
-        <div class="places form content">
+    <div class="places form content">
         <?= $this->Form->create($place) ?>
-            <fieldset>
-                <legend><?= __('Add Place') ?></legend>
-                <?php
-                    echo $this->Form->control('subcategory_id', ['options' => $subcategories]);
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('address', ['id' => 'autocomplete', 'type' => 'text', 'placeholder' => 'Enter an address']);
-                    echo $this->Form->control('description');
-                ?>
-            </fieldset>
-        <?= $this->Form->button(__('Submit')) ?>
-        <?= $this->Form->end() ?>
+        <fieldset>
+            <!-- Subcategory -->
+            <label for="subcategory-id" class="form-label">Subcategory <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('subcategory_id', [
+                'label' => false,
+                'options' => $subcategories,
+                'class' => 'form-control pixel-input mt-2 mb-4',
+                'required' => true,
+                'id' => 'subcategory-id'
+            ]) ?>
+
+            <!-- Name -->
+            <label for="place-name" class="form-label">Place Name <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('name', [
+                'label' => false,
+                'class' => 'form-control pixel-input mt-2 mb-4',
+                'required' => true,
+                'id' => 'place-name'
+            ]) ?>
+
+            <!-- Address -->
+            <label for="autocomplete" class="form-label">Address <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('address', [
+                'label' => false,
+                'id' => 'autocomplete',
+                'type' => 'text',
+                'placeholder' => 'Enter an address',
+                'class' => 'form-control pixel-input mt-2 mb-4',
+                'required' => true
+            ]) ?>
+
+            <!-- Description -->
+            <label for="place-description" class="form-label">Description</label>
+            <?= $this->Form->control('description', [
+                'label' => false,
+                'id' => 'place-description',
+                'class' => 'form-control pixel-input mt-2 mb-4'
+            ]) ?>
+        </fieldset>
+
+        <div class="d-flex justify-content-end">
+            <?= $this->Form->button('Submit') ?>
         </div>
+        <?= $this->Form->end() ?>
     </div>
 </div>
