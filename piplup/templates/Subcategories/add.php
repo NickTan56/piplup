863aedb17fa0481085f71a954df8ea281fb0b3c4
Nicolas Stevan Tan <nicolas562003@gmail.com>
2025-04-25 21:49:57 +1000

    Subcategory Add UI

--- piplup/templates/Subcategories/add.php
+++ piplup/templates/Subcategories/add.php
@@ -5,25 +5,46 @@
  * @var \Cake\Collection\CollectionInterface|string[] $categories
  */
 ?>
+<?= $this->Html->link('Back', '/new-menu', ['class' => 'px-1 pt-1', 'escape' => false]) ?>
+
+<!-- Piplup Header -->
+<div class="d-flex flex-wrap justify-content-start align-items-center">
+    <?= $this->Html->image('piplup.png', [
+        'class' => 'mb-3',
+        'style' => 'width:113px; height:113px;',
+        'alt' => 'Piplup'
+    ]) ?>
+    <h1 class="h3 mb-3 heading-title">New Subcategory?</h1>
+</div>
+
+<!-- Form Container -->
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Subcategories'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+    <div class="subcategories form content">
+        <?= $this->Form->create($subcategory) ?>
+        <fieldset>
+            <!-- Category Selection -->
+            <label for="category-id" class="form-label">Parent Category <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('category_id', [
+                'label' => false,
+                'options' => $categories,
+                'class' => 'form-control pixel-input mt-2 mb-4',
+                'required' => true,
+                'id' => 'category-id'
+            ]) ?>
+
+            <!-- Subcategory Name -->
+            <label for="subcategory-name" class="form-label">Subcategory Name <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('name', [
+                'label' => false,
+                'class' => 'form-control pixel-input mt-2 mb-4',
+                'required' => true,
+                'id' => 'subcategory-name'
+            ]) ?>
+        </fieldset>
+
+        <div class="d-flex justify-content-end">
+            <?= $this->Form->button('Submit') ?>
         </div>
-    </aside>
-    <div class="column column-80">
-        <div class="subcategories form content">
-            <?= $this->Form->create($subcategory) ?>
-            <fieldset>
-                <legend><?= __('Add Subcategory') ?></legend>
-                <?php
-                    echo $this->Form->control('category_id', ['options' => $categories]);
-                    echo $this->Form->control('name');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
+        <?= $this->Form->end() ?>
     </div>
 </div>
