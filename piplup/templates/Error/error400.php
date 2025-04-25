bb6927c423e35c8c13c8eebe1ea7d56f3d2b04fc
Nicolas Stevan Tan <nicolas562003@gmail.com>
2025-04-26 08:08:12 +1000

    Error pages

--- piplup/templates/Error/error400.php
+++ piplup/templates/Error/error400.php
@@ -6,21 +6,23 @@
  */
 use Cake\Core\Configure;
 
-$this->layout = 'error';
+$this->layout = 'default';
 
 if (Configure::read('debug')) :
     $this->layout = 'dev_error';
-
     $this->assign('title', $message);
     $this->assign('templateName', 'error400.php');
-
     $this->start('file');
     echo $this->element('auto_table_warning');
     $this->end();
 endif;
 ?>
-<h2><?= h($message) ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
-</p>
+
+<div class="text-center mt-5" style="font-family: 'Pixelify Sans', sans-serif;">
+    <?= $this->Html->image('piplup.png', ['style' => 'width: 120px; height: auto;', 'alt' => 'Piplup']) ?>
+    <h1 class="mt-4" style="font-size: 2.5rem;">Error 404</h1>
+    <p class="mb-4" style="font-size: 1.25rem;">Piplup hasn't been here before!</p>
+    <?= $this->Html->link('Back', '/', [
+        'class' => 'pixel-button blue',
+    ]) ?>
+</div>
--- piplup/templates/Error/error500.php
+++ piplup/templates/Error/error500.php
@@ -7,30 +7,23 @@
 use Cake\Core\Configure;
 use Cake\Error\Debugger;
 
-$this->layout = 'error';
+$this->layout = 'default';
 
 if (Configure::read('debug')) :
     $this->layout = 'dev_error';
-
     $this->assign('title', $message);
     $this->assign('templateName', 'error500.php');
-
     $this->start('file');
-?>
-<?php if ($error instanceof Error) : ?>
-    <?php $file = $error->getFile() ?>
-    <?php $line = $error->getLine() ?>
-    <strong>Error in: </strong>
-    <?= $this->Html->link(sprintf('%s, line %s', Debugger::trimPath($file), $line), Debugger::editorUrl($file, $line)); ?>
-<?php endif; ?>
-<?php
     echo $this->element('auto_table_warning');
-
     $this->end();
 endif;
 ?>
-<h2><?= __d('cake', 'An Internal Error Has Occurred.') ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= h($message) ?>
-</p>
+
+<div class="text-center mt-5" style="font-family: 'Pixelify Sans', sans-serif;">
+    <?= $this->Html->image('piplup.png', ['style' => 'width: 120px; height: auto;', 'alt' => 'Piplup']) ?>
+    <h1 class="mt-4" style="font-size: 2.5rem;">Error 500</h1>
+    <p class="mb-4" style="font-size: 1.25rem;">Piplup is feeling a little dizzy... something went wrong!</p>
+    <?= $this->Html->link('Back', '/', [
+        'class' => 'pixel-button blue',
+    ]) ?>
+</div>
