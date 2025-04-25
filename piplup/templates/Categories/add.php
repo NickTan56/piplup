f3116a6b5fc07204cd0ee9309bd74652684cff0d
Nicolas Stevan Tan <nicolas562003@gmail.com>
2025-04-25 17:17:49 +1000

    Add new Category ui

--- piplup/templates/Categories/add.php
+++ piplup/templates/Categories/add.php
@@ -4,24 +4,29 @@
  * @var \App\Model\Entity\Category $category
  */
 ?>
+<?= $this->Html->link('Back', '/new-menu', ['class' => 'px-1 pt-1', 'escape' => false]) ?>
+<!-- Piplup Header -->
+<div class="d-flex flex-wrap justify-content-start align-items-center">
+    <?= $this->Html->image('piplup.png', [
+        'class' => 'mb-3',
+        'style' => 'width:113px; height:113px;',
+        'alt' => 'Piplup'
+    ]) ?>
+    <h1 class="h3 mb-3 heading-title">New Category?</h1>
+</div>
+<!-- Form Container -->
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Categories'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column column-80">
-        <div class="categories form content">
-            <?= $this->Form->create($category) ?>
-            <fieldset>
-                <legend><?= __('Add Category') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
+    <div class="categories form content">
+        <?= $this->Form->create($category) ?>
+        <fieldset>
+            <?= $this->Form->control('name', [
+                'label' => 'Category Name',
+                'class' => 'form-control pixel-input mt-2 mb-4',
+                'style' => 'max-width: 400px;',
+            ]) ?>
+        </fieldset>
+        <?= $this->Form->button('Submit') ?>
+        <?= $this->Form->end() ?>
     </div>
 </div>
+
