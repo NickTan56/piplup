c77bf2f8e44cc63a902389e772715ce4cae914c3
Nicolas Stevan Tan <nicolas562003@gmail.com>
2025-04-27 09:30:17 +1000

    Edit pages UI

--- piplup/templates/Categories/edit.php
+++ piplup/templates/Categories/edit.php
@@ -4,29 +4,36 @@
  * @var \App\Model\Entity\Category $category
  */
 ?>
+
+<?= $this->Html->link('Back', $this->request->referer(), ['class' => 'px-1 pt-1', 'escape' => false]) ?>
+
+<!-- Piplup Header -->
+<div class="d-flex flex-wrap justify-content-start align-items-center">
+    <?= $this->Html->image('piplup.png', [
+        'class' => 'mb-3',
+        'style' => 'width:113px; height:113px;',
+        'alt' => 'Piplup'
+    ]) ?>
+    <h1 class="h3 mb-3 heading-title">Edit Category</h1>
+</div>
+
+<!-- Form Container -->
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $category->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $category->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Categories'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+    <div class="categories form content">
+        <?= $this->Form->create($category) ?>
+        <fieldset>
+            <label for="name" class="form-label">Category Name <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('name', [
+                'label' => false,
+                'placeholder' => 'Enter the name',
+                'class' => 'form-control pixel-input',
+                'required' => true,
+                'id' => 'name',
+            ]) ?>
+        </fieldset>
+        <div class="d-flex justify-content-end mt-3">
+            <?= $this->Form->button('Submit') ?>
         </div>
-    </aside>
-    <div class="column column-80">
-        <div class="categories form content">
-            <?= $this->Form->create($category) ?>
-            <fieldset>
-                <legend><?= __('Edit Category') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
+        <?= $this->Form->end() ?>
     </div>
 </div>
--- piplup/templates/Subcategories/edit.php
+++ piplup/templates/Subcategories/edit.php
@@ -5,30 +5,48 @@
  * @var string[]|\Cake\Collection\CollectionInterface $categories
  */
 ?>
+
+<?= $this->Html->link('Back', $this->request->referer(), ['class' => 'px-1 pt-1', 'escape' => false]) ?>
+
+<!-- Piplup Header -->
+<div class="d-flex flex-wrap justify-content-start align-items-center">
+    <?= $this->Html->image('piplup.png', [
+        'class' => 'mb-3',
+        'style' => 'width:113px; height:113px;',
+        'alt' => 'Piplup'
+    ]) ?>
+    <h1 class="h3 mb-3 heading-title">Edit Subcategory</h1>
+</div>
+
+<!-- Form Container -->
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $subcategory->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $subcategory->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Subcategories'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+    <div class="subcategories form content">
+        <?= $this->Form->create($subcategory) ?>
+        <fieldset>
+            <!-- Category Selection -->
+            <label for="category-id" class="form-label">Parent Category <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('category_id', [
+                'label' => false,
+                'options' => $categories,
+                'class' => 'form-control pixel-input',
+                'required' => true,
+                'id' => 'category-id'
+            ]) ?>
+
+            <!-- Subcategory Name -->
+            <label for="subcategory-name" class="form-label mt-3">Subcategory Name <span class="required-asterisk">*</span></label>
+            <?= $this->Form->control('name', [
+                'label' => false,
+                'placeholder' => 'Enter the name',
+                'class' => 'form-control pixel-input',
+                'required' => true,
+                'id' => 'subcategory-name'
+            ]) ?>
+        </fieldset>
+
+        <div class="d-flex justify-content-end mt-3">
+            <?= $this->Form->button('Submit') ?>
         </div>
-    </aside>
-    <div class="column column-80">
-        <div class="subcategories form content">
-            <?= $this->Form->create($subcategory) ?>
-            <fieldset>
-                <legend><?= __('Edit Subcategory') ?></legend>
-                <?php
-                    echo $this->Form->control('category_id', ['options' => $categories]);
-                    echo $this->Form->control('name');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
+        <?= $this->Form->end() ?>
     </div>
 </div>
